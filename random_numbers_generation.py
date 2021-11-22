962bbb8e65fc189a550955122c69ec17aad9112b
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-11-21 21:32:54 -0800

    Add files via upload

---
+++ random_numbers_generation.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 09:58:58 2021
+
+@author: admin
+"""
+
+import random
+data=[]
+for ii in range(20):
+    temp=random.randint(1,100)
+    data.append(temp)
+print(data)
