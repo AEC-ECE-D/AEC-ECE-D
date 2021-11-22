3cb742e8d8149f1ae3e3017ca3f6ab7cab154a8f
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-11-21 22:09:48 -0800

    Add files via upload

---
+++ single digit.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 14:48:05 2021
+
+@author: admin
+"""
+
+n=int(input("enter a number"))
+if n==0:
+    print("Zero")
+elif n==1:
+    print("one")
+elif n==2:
+    print("tw0")
+elif n==3:
+    print("three")
+elif n==4:
+    print("four")
+elif n==5:
+    print("five")
+elif n==6:
+    print("six")
+elif n==7:
+    print("seven")
+elif n==8:
+    print("eight")
+elif n==9:
+    print("nine")
+else:
+    print("it is not single number")
+    
