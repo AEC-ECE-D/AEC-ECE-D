24fc8d7ba9c09522a24f3d918fcac0e1807b6e20
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2021-12-05 23:04:29 -0800

    Add files via upload

---
+++ factors_count.py
@@ -0,0 +1,53 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+100
+9
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+36
+9
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+1000
+16
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+100
+93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+10
+3628800
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+2
+2
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+3
+6
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+4
+24
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+5
+120
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+enter a number100
+Traceback (most recent call last):
+  File "C:\Users\admin\factor_count.py", line 8, in <module>
+    count1=number_of_factors(num1)
+NameError: name 'number_of_factors' is not defined. Did you mean: 'number_0f_factors'?
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+enter a number100
+number of factors= 1
+
+==================== RESTART: C:\Users\admin\factor_count.py ===================
+enter a number100
+number of factors= 9
