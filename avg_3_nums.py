488f9cb05cc35fc01a750a1efd2084996767e0a5
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-02-11 20:31:02 +0530

    Add files via upload

---
+++ avg_3_nums.py
@@ -0,0 +1,7 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter three number"))
+total=num1+num2+num3
+avg=total/3
+print(f"{num1}+{num2}+{num3}={total}")
+print(f"average({num1},{num2},{num3})={avg:.2f}")
